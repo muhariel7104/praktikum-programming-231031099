59847a120e173a66d1453b5a5e8367718df014ac
muhariel7104 <153707715+muhariel7104@users.noreply.github.com>
2023-12-19 13:03:16 +0800

    Add files via upload

---
+++ praktikum-d1.py
@@ -0,0 +1,284 @@
+print('praktikum-d1')
+print()
+print('nama lengkap : muh ariel')
+print('nim          : 231931099')
+print('prodi        : sistem informasi')
+
+angkatan = 2023
+print('angkatan adalah',angkatan)
+
+print()
+a = 20
+print('data adalah =',a)
+print('tipenya adalah =',type(a))
+
+print()
+b = 20.0
+print('data adalah =',b)
+print('tipenya adalah =',type(b))
+
+print()
+kampus = 'institu teknologi bj habibi'
+print('data adalah =',kampus)
+print('tipenya adalah =',type(kampus))
+print()
+log = True
+print('data adalah =',log)
+print('tipenya adalah =',type(log))
+print()
+c = complex(5,8)
+print('data adalah =',c)
+print('tipenya adalah =',type(c))
+print()
+p = 20
+q = 5
+hasil = p + q
+print('hasil',p,'+',q,'adalah',hasil)
+print()
+p = 20
+q = 5
+hasil = p + q
+print('hasil',p,'+',q,'adalah',hasil)
+print()
+p = 20
+q = 5
+hasil = p - q
+print('hasil',p,'-',q,'adalah',hasil)
+print()
+p = 20
+q = 5
+hasil = p / q
+print('hasil',p,'x',q,'adalah',hasil)
+print
+print()
+p = 20
+q = 5
+hasil = p / q
+print('hasil',p,'/',q,'adalah',hasil)
+
+p = 20
+q = 5
+hasil = p / q
+print('hasil',p,'/',q,'adalah',hasil)
+print()
+p = 37
+q = 31
+hasil = p % q
+print('hasil',p,'%',q,'adalah',hasil)
+print()
+p = 20
+q = 3
+hasil = p / q
+print('hasil',p,'/',q,'adalah',hasil)
+print()
+a = 20
+print('nilai a adalah',a)
+a%= 7  #ini perintah a = a % 7
+print('nilai a % = 7 menjadi',a)
+print()
+a = 20
+print('nilai a adalah',a)
+a//= 7  #ini perintah a = a // 7
+print('nilai a // = 7 menjadi',a)
+print() #ini oprasi OR
+log = True
+print('nilai log adalah',log)
+log |=False  #ini perintah log=False log
+print('nila log|=False menjadi',log)
+print() #ini operasi OR
+log = True
+print('nilai log adalah',log)
+log &=True  #ini perintah log= log & False
+print('nila log|=False menjadi',log)
+print() #ini oprasi XOR
+log = True
+print('nilai log adalah',log)
+log |=True  #ini perintah log = log ^ True
+print('nila log^=True menjadi',log)
+print()  #ini oprasi shifting
+bi = 0b0100
+print('nilai bi =',format(bi,'04b'))
+bi<<=1  #menggeser digit ke kanan 1 kali
+print('nilai bi<<=1 menjadi',format(bi,'04b'))
+
+
+print()
+a = 20
+hasil = a>15
+print('hasil',a>'15 adalah', hasil)
+
+print()
+a = 20
+hasil = a==15
+print('hasil',a,'15 adalah', hasil)
+print()
+print('hasil pembagian')
+print()
+a = 20
+hasil = a!=15
+print('hasil',a,'!=15 adalah', hasil)
+
+print()
+a = 20
+hasil = a<=15
+print('hasil',a,'<=15 adalah', hasil)
+
+print()
+a = 20;
+hasil = a>=15;
+print('hasil',a,'>=15 adalah', hasil)
+
+print()
+a = 20;
+hasil = a<=15;
+print('hasil',a,'<=15 adalah', hasil)
+print('pratikum-d1')
+print()
+print('nama lengkap : Muh Ariel')
+print('nim          : 231031099')
+print('prodi        : sistem informasi')
+
+angkatan = 2023 
+print('angkatan adalah',angkatan)
+
+print()
+a = 20
+print('data adalah =',a)
+print('tipenya adalah =',type(a))
+
+print()
+b = 20.0
+print('data adalah =',b)
+print('tipenya adalah =',type(b))
+
+print()
+kampus = 'institut teknologi bj habibie'
+print('data adalah =',kampus)
+print('tipenya adalah =',type(kampus))
+
+print()
+log = True
+print('data adalah =',log)
+print('tipenya adalah =',type(log))
+
+print()
+c = complex(5,8)
+print('data adalah =',c)
+print('tipenya adalah =',type(c))
+
+print()
+p = 20
+q = 5
+hasil = p + q 
+print('hasil',p,'+',q,'adalah',hasil)
+
+print()
+p = 20
+q = 5
+hasil = p - q 
+print('hasil',p,'-',q,'adalah',hasil)
+
+print()
+p = 20
+q = 5
+hasil = p * q 
+print('hasil',p,'x',q,'adalah',hasil)
+
+print()
+p = 20
+q = 3
+hasil = p / q 
+print('hasil',p,'/',q,'adalah',hasil)
+
+print()
+p = 3
+q = 2
+hasil = p**q 
+print('hasil',p,'^',q,'=',hasil)
+
+print()
+p = 37
+q = 31
+hasil = p % q 
+print('hasil',p,'%',q,'=',hasil)
+
+print()
+p = 37
+q = 31
+hasil = p // q 
+print('hasil',p,'//',q,'=',hasil)
+
+print()
+a = 20
+print('nilai,a adalah', a)
+a %= 7          #ini perintah a = a % 7
+print('nilai a%=7 menjadi', a)
+
+print()
+a = 20
+print('nilai,a adalah', a)
+a //= 7          #ini perintah a = a // 7
+print('nilai a//=7 menjadi', a)
+
+print() #ini operasi QR
+log =True
+print('nilai log adalah',log)
+log |=False #ini perintah log=False log
+print('nilai log|=False menjadi',log)
+
+print() #ini operasi AND
+log =True
+print('nilai log adalah',log)
+log &=True #ini perintah log=log & false
+print('nilai log&=True menjadi',log)
+
+print() #ini operasi XOR
+log =True
+print('nilai log adalah',log)
+log ^=True #ini perintah log=log ^ True
+print('nilai log ^=True menjadi',log)
+
+print() # ini operasi shifting
+bi = 0b0100
+print('nilai bi =',format(bi,'04b'))
+bi >>=1 #menggeser digit ke kenan 1 kali
+print('nilai bi >>=1 menjadi',format(bi,'04b'))
+
+print() # ini operasi shifting
+bi = 0b0100
+print('nilai bi =',format(bi,'04b'))
+bi <<=1 #menggeser digit ke kiri 1 kali
+print('nilai bi <<=1 menjadi',format(bi,'04b'))
+
+print()
+a = 20
+hasil = a>15
+print('hasil',a,'>15 adalah', hasil)
+
+print()
+a = 20
+hasil = a==15
+print('hasil',a,'==15 adalah', hasil)
+
+print()
+a = 20
+hasil = a!=15
+print('hasil',a,'!=15 adalah', hasil)
+
+print()
+a = 20
+hasil = a<=15
+print('hasil',a,'<=15 adalah', hasil)
+
+print()
+a = 20;
+hasil = a>=15;
+print('hasil',a,'>=15 adalah', hasil)
+
+print()
+a = 20;
+hasil = a<=15;
+print('hasil',a,'<=15 adalah', hasil)
+
+      
+
